a76e099257ff84baf42ecb2845dc91da34c3336b
= <daniloagostinhosilva@gmail.com>
2023-05-24 14:57:51 -0300

    Criando preview e upload de imagens

--- src/components/registration-form/RegistrationForm.js
+++ src/components/registration-form/RegistrationForm.js
@@ -14,14 +14,28 @@ class RegistrationForm extends HTMLElement {
     }
 }
 
+let uploadedAvatar;
+
 const setAvatar = () => {
     const imgAvatar = document.querySelector('.avatar');
-    if(imgAvatar) {
+    if (imgAvatar) {
         imgAvatar.src = 'src/assets/images/avatar.png'
     }
 }
 
+const uploadAvatar = (event) => {
+    if(event.target.files && event.target.files.length > 0) {
+        const file = event.target.files[0];
 
+        const reader = new FileReader();
+
+        reader.onload = () => (document.querySelector('.avatar').src = reader.result)
+
+        reader.readAsDataURL(file);
+
+        uploadedAvatar = file;
+    }
+}
 
 if ('customElements' in window) {
     customElements.define('app-registration-form', RegistrationForm)
