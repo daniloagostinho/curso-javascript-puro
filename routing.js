d43ca09c229f1abfaab31fac35633a068bb2181c
= <daniloagostinhosilva@gmail.com>
2023-05-27 09:47:37 -0300

    Criando o roteamento da aplicação

---
+++ routing.js
@@ -0,0 +1,33 @@
+const routes = {
+    '/': '<app-login></app-login>',
+    '/index.html': '<app-login></app-login>',
+    '/dashboard': '<app-dashboard></app-dashboard>'
+}
+
+const renderRoute = async (route) => {
+    const routeContent = await canAccesRoute(route) ? routes[route] : routes['/'];
+    document.getElementById('main-page').innerHTML = routeContent;
+}
+
+const navigateTo = (pathname) => {
+    renderRoute(pathname);
+}
+
+const isAuthenticated = async () => {
+    const token = localStorage.getItem('token');
+
+    if (!token) return false;
+
+    const response = await window.validateToken(`${window.apiURL}/validate/token`, token);
+    const data = await response.json();
+
+    return data.isValidToken;
+}
+
+const canAccesRoute = async (route) => {
+    if (route !== '/dashboard') return true;
+    return await isAuthenticated();
+}
+
+
+renderRoute(window.location.pathname)
