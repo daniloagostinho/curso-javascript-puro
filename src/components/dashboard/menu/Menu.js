dfb31f9503de3aa1f6f5722b5b0cad577810d9dc
= <daniloagostinhosilva@gmail.com>
2023-05-28 12:13:38 -0300

    Exibindo imagem em base64

--- src/components/dashboard/menu/Menu.js
+++ src/components/dashboard/menu/Menu.js
@@ -8,7 +8,26 @@ class Menu extends HTMLElement {
     }
 
     connectedCallback() {
+        setTimeout(() => {
+            getImageUser();
+        }, 1000)
     }
+}
+
+const getImageUser = () => {
+    const img = document.querySelector('.profile-img');
+
+    const nameImage = JSON.parse(localStorage.getItem('userInfo'));
+
+    window.downloadImage(`${window.apiURL}/download/image`, nameImage.image)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            let url = 'data:image/jpg;base64, ' + response.image;
+            img.src = url;
+        })
+
+
 }
 
 if ('customElements' in window) {
