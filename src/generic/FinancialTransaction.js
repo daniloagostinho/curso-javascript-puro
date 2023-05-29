b8b5e645a8d6df850b1928f733558769d59855ad
= <daniloagostinhosilva@gmail.com>
2023-05-29 11:46:23 -0300

    Criando método de abrir modal de adicionar receitas

--- src/generic/FinancialTransaction.js
+++ src/generic/FinancialTransaction.js
@@ -1,3 +1,9 @@
 const capitalizeFirstLetter = (string) => {
     return string.substring(0, 1).toUpperCase() + string.substring(1);
 }
+
+const openDialogAddFinancial = (financialType) => {
+    const dialogSelector = (financialType === 'income') ? '.add-income-modal' : '.add-expense-modal';
+    const dialog = document.querySelector(dialogSelector);
+    dialog.click();
+}
