9514f865733446e127b3e37cf735298338dda242
= <daniloagostinhosilva@gmail.com>
2023-05-29 11:46:52 -0300

    Criando modais de categoria da receita e método de pagamento

--- src/utils/GlobalVariables.js
+++ src/utils/GlobalVariables.js
@@ -10,4 +10,73 @@ window.gender = [
     }
 ];
 
+window.typeIncome = [
+    {
+        name: 'Salário'
+    },
+    {
+        name: 'Rendimentos autônomos'
+    },
+    {
+        name: 'Rendimentos passivos'
+    },
+    {
+        name: 'Bônus'
+    },
+    {
+        name: 'Investimento',
+    },
+    {
+        name: 'Empréstimo',
+    },
+    {
+        name: 'Férias',
+    },
+    {
+        name: '13 Sálario',
+    },
+    {
+        name: 'PLR',
+    },
+    {
+        name: 'Aposentaria',
+    },
+    {
+        name: 'Aluguel',
+    },
+    {
+        name: 'Outros',
+    }
+]
+
+window.paymentMethod = [
+    {
+        name: 'Dinheiro'
+    },
+    {
+        name: 'Cheque',
+    },
+    {
+        name: 'Cartão de débito',
+    },
+    {
+        name: 'Cartão de crédito',
+    },
+    {
+        name: 'Pix',
+    },
+    {
+        name: 'Transferência bancária',
+    },
+    {
+        name: 'Ordens de pagamento',
+    },
+    {
+        name: 'Boleto bancário ',
+    },
+    {
+        name: 'Outros'
+    }
+]
+
 window.apiURL = 'http://localhost:3000';
