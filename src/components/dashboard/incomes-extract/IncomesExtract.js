f16a46de6277257d10321fa4880e83c8ba0eb447
= <daniloagostinhosilva@gmail.com>
2023-06-15 14:12:31 -0300

    Criando o componente de extrato de receitas

---
+++ src/components/dashboard/incomes-extract/IncomesExtract.js
@@ -0,0 +1,18 @@
+class IncomesExtract extends HTMLElement {
+    constructor() {
+        super();
+
+        fetch('src/components/dashboard/incomes-extract/IncomesExtract.html')
+            .then(response => response.text())
+            .then(text => this.innerHTML = text)
+    }
+
+    connectedCallback() {
+        setTimeout(() => {
+        }, 3000)
+    }
+}
+
+if ('customElements' in window) {
+    customElements.define('app-incomes-extract', IncomesExtract)
+}
