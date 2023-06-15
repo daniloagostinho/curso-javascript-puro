5aa6e8eb8cfcd4809022ac2f64a16025a9204691
= <daniloagostinhosilva@gmail.com>
2023-06-15 17:51:37 -0300

    Listando receitas no componente de extrato - parte 4

--- src/generic/ExtractTransaction.js
+++ src/generic/ExtractTransaction.js
@@ -9,7 +9,7 @@ const createHTMLElement = (tag, className, innerHTML = '') => {
 }
 
 const populateTransactionCards = (financialType, arr, arrFull) => {
-    const otions = {
+    const options = {
         day: '2-digit',
         month: '2-digit',
         year: 'numeric'
@@ -32,7 +32,7 @@ const populateTransactionCards = (financialType, arr, arrFull) => {
 
     const currentExtractArr = JSON.stringify(arr);
 
-    if (currentExtractArr === lastExtractArr) return;
+    // if (currentExtractArr === lastExtractArr) return;
 
     const timeline = document.querySelector(`.my-timeline-${financialType}`);
     timeline.innerHTML = '';
@@ -41,10 +41,33 @@ const populateTransactionCards = (financialType, arr, arrFull) => {
     document.querySelector(`.no-result-${financialType}-extract`).style.display = 'none';
     document.querySelector(`.my-pagination-${financialType}-extract`).style.display = 'block';
 
-    const totalFinancialType = arrFull.reduce((accumulator, currentValue) => accumulator + currentValue.total, 0);
-    document.querySelector(`.${financialType}-balance`).innerHTML = `Saldo total: ${currencyValue(totalFinancialType)}`;
+    // const totalFinancialType = arrFull.reduce((accumulator, currentValue) => accumulator + currentValue.total, 0);
+    // document.querySelector(`.${financialType}-balance`).innerHTML = `Saldo total: ${currencyValue(totalFinancialType)}`;
 
-    
+   arr.result.forEach(transactionType => {
+        const timelineItem = createHTMLElement('div', 'timeline-item');
+        timelineItem.appendChild(createHTMLElement('div', 'my-line'));
+        timelineItem.appendChild(createHTMLElement('div', 'my-dot'));
+
+        const timelineContent = createHTMLElement('div', 'my-timeline-content');
+        timelineContent.appendChild(createHTMLElement('h5', '', capitalizeFirstLetter(transactionType.month)))
+       
+
+        transactionType.details.forEach(detail => {
+            const detailDate = new Date(detail.dueDate);
+            const category = financialType === 'income' ? detail.income : detail.expense;
+
+            timelineContent.appendChild(createHTMLElement('p', '', `<strong>${dateLabel}:</strong> ${detailDate.toLocaleDateString('pt-BR', options)}`));
+            timelineContent.appendChild(createHTMLElement('p', '', `<strong>${labelCategory}:</strong> ${category}`));
+            timelineContent.appendChild(createHTMLElement('p', '', `<strong>Valor:</strong> ${currencyValue(detail.value)}`));
+
+
+            console.log(timelineContent)
+        })
+
+        timelineItem.appendChild(timelineContent)
+        timeline.appendChild(timelineItem)
+   });
 }
 
 const checkClickedExtract = (financialType) => {
