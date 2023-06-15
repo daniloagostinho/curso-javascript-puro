877b8874f36afc348d3f42b29ff936d7039b2e06
= <daniloagostinhosilva@gmail.com>
2023-06-15 17:10:34 -0300

    Listando receitas no componente de extrato - parte 1

--- src/generic/ExtractTransaction.js
+++ src/generic/ExtractTransaction.js
@@ -8,3 +8,24 @@ const createHTMLElement = (tag, className, innerHTML = '') => {
     return element;
 }
 
+const populateTransactionCards = (financialType, arr, arrFull) => {
+    const otions = {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    };
+
+    const labelCategory = 'Categoria';
+
+    const dateLabel = financialType === 'income' ? 'Data de entrada' : 'Data de vencimento';
+
+    // Verifica se array está vazio
+    if (!arr.lentgh) {
+        const timeline = document.querySelector(`.my-timeline-${financialType}`);
+        timeline.innerHTML = '';
+
+        document.querySelector(`.no-result-${financialType}-extract`).style.display = 'block';
+        document.querySelector(`.my-pagination-${financialType}-extract`).style.display = 'none';
+        document.querySelector(`.${financialType}-balance`).innerHTML = '';
+    }
+}
